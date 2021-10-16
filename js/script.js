99449e2ad4bbc300d2b4289848cc84b445b4c2a6
jaime.nunes <jaimenunesleal@gmail.com>
2021-10-16 18:34:53 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,32 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView:1,
+    spaceBetween: 10,
+    slidesPerGroup: 2,
+    breakpoints: {
+        // when window width is >= 320px
+        320: {
+          slidesPerView: 2,
+          spaceBetween: 20
+        },
+        // when window width is >= 480px
+        480: {
+          slidesPerView: 3,
+          spaceBetween: 30
+        },
+        // when window width is >= 640px
+        800: {
+          slidesPerView: 6,
+          spaceBetween: 20
+        }
+    },
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
---
+++ readme.md
@@ -0,0 +1,4 @@
+#Netflix Clone
+
+Projeto utilizando HTML5, CSS3, Javascript com Swiperjs.
+
